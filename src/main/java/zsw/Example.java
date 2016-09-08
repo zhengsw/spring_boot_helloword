2e425c2c77a01d62330d881b12a6bbf4141b740c
zhengsw <326002473@qq.com>
2016-09-08 15:59:39 +0800

    add restful controller

--- src/main/java/zsw/Example.java
+++ src/main/java/zsw/Example.java
@@ -1,15 +1,22 @@
 package zsw;
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @EnableAutoConfiguration
+@RequestMapping("zsw")
 public class Example {
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     String home() {
         return "Hello World!";
+    }
+    @RequestMapping("/hi/{name}")
+    String hi(@PathVariable String name){
+        return "hi ".concat(name);
     }
 
     public static void main(String[] args) throws Exception {
